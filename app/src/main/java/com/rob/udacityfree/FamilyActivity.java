243e541c7eb83f155aefbf2deadd9b49bc2f8165
strawHatRobby <mailrobinfrancis@gmail.com>
2016-12-08 11:04:51 +0530

    Created different activites and hooks to access them fron front page

---
+++ app/src/main/java/com/rob/udacityfree/FamilyActivity.java
@@ -0,0 +1,14 @@
+package com.rob.udacityfree;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class FamilyActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_family);
+    }
+
+}
--- app/src/main/java/com/rob/udacityfree/MainActivity.java
+++ app/src/main/java/com/rob/udacityfree/MainActivity.java
@@ -1,13 +1,61 @@
 package com.rob.udacityfree;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView mNumberActivityTextView;
+    private  TextView mFamilyActivityTextView;
+    private TextView mPhrasesActivityTextView;
+    private  TextView mColorsActivityTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mNumberActivityTextView  = (TextView)findViewById(R.id.numbers_acitivity_text_view);
+        mColorsActivityTextView = (TextView)findViewById(R.id.colors_activity_text_view);
+        mFamilyActivityTextView = (TextView)findViewById(R.id.family_activity_text_view);
+        mPhrasesActivityTextView = (TextView)findViewById(R.id.phrases_activity_text_view);
+
+        mNumberActivityTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, NumbersActivity.class);
+                startActivity(i);
+            }
+        });
+
+        mFamilyActivityTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, FamilyActivity.class);
+                startActivity(i);
+            }
+        });
+
+        mColorsActivityTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, ColorsActivity.class);
+                startActivity(i);
+            }
+        });
+
+        mPhrasesActivityTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, PhrasesActivity.class);
+                startActivity(i);
+            }
+        });
     }
+
+
+
+
 }
